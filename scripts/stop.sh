7344aa5f5714751ded551bf990f980e466f29a98
leejunsu <qkfhtit@gmail.com>
2021-08-10 13:55:46 +0900

    무중단 배포 에어 1.2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readlink -f $0) # 현재 stop.sh가 속해 있는 경로를 찾습니다.
-                          # 하단의 코드와 같이 profile.sh의 경로를 찾기 위해 사용됩니다.
+ABSPATH=$(readlink -f $0)   # 현재 stop.sh가 속해 있는 경로를 찾습니다.
+                            # 하단의 코드와 같이 profile.sh의 경로를 찾기 위해 사용됩니다.
 ABSDIR=$(dirname $ABSPATH)  # 자바로 보면 일종의 import구문입니다.
                             # 해당 코드로 인해 stop.sh에서도 profile.sh의 여러 function을 사용할 수 있게 됩니다.
-source ${ABSDIR} / profile.sh
+source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
@@ -19,5 +19,4 @@ else
   kill -15 ${IDLE_PID}
   sleep 5
 fi
-
 
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -5,12 +5,12 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
-  IDLE_PORT=$(find_idle_port)
+    IDLE_PORT=$(find_idle_port)
 
-  echo "> 전환할 port: $IDLE_PORT"
-  echo "> port 전환"
-  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
-  echo "> 엔진엑스 reload"
-  sudo service nginx reload
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 전환"
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
+    echo "> 엔진엑스 reload"
+    sudo systemctl reload nginx
 }
